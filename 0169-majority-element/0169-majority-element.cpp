ff846c329b1bf64b1876c5bb0222728991730f2c
Aryan Tiwari <91017115+MrAryan8728@users.noreply.github.com>
2023-09-08 20:17:49 +0530

    Time: 4 ms (99.09%), Space: 19.7 MB (7.30%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,5 +1,25 @@
 class Solution {
 public:
+    /*
+    Approach
+lets take the count = 0 and starting from the first element(nums[0]) of the array which is stored in el variable , we are iterating through the loop. if the elemnts of the array while iterating through the loop is equal to the element stored earlier in it then increase the count by 1 , otherwise decrease it by 1. also , most important while doing this there will be a point when the count ill be zero then in that case the the next element is taken and the count again starts from 1.
+
+given array: [2 2 1 1 2 2]
+
+take el=2
+
+cnt=0->1
+the next el is also 2 so again it will increase
+cnt=2
+
+now the next el is 1 so it will decrease
+el=1 (el changes from 2 to 1)
+cnt=1
+
+again decrease
+cnt=0 now when is zero the next element we'll get is 2 which will make the count 1 again
+and so its goes on.....
+    */
     int majorityElement(vector<int>& nums) {
         int count = 0,ans = -1;
         for(int i = 0;i < nums.size();i++){
